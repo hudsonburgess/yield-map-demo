6a1979b18b523d4357e63bc6c11d99235cd463e0
Hudson Burgess <hudsonburgess7@gmail.com>
2019-03-08 11:56:21 -0500

    first commit

---
+++ draw-map.js
@@ -0,0 +1,339 @@
+const lipData = [{
+        "fieldName": "LIP",
+        "sampleName": "LIP12",
+        "sampleLat": 37.53501841,
+        "sampleLon": -76.8828617,
+        "p_2013": 53,
+        "p_removal_2013": 40.66,
+        "p_rec_2013": 40,
+        "p_expected_2014": 52.34,
+        "p_actual_2014": 105
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP13",
+        "sampleLat": 37.53439336,
+        "sampleLon": -76.88285336,
+        "p_2013": 61,
+        "p_removal_2013": 39.14,
+        "p_rec_2013": 40,
+        "p_expected_2014": 61.86,
+        "p_actual_2014": 93
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP21",
+        "sampleLat": 37.53138169,
+        "sampleLon": -76.88193823,
+        "p_2013": 77,
+        "p_removal_2013": 39.14,
+        "p_rec_2013": 0,
+        "p_expected_2014": 37.86,
+        "p_actual_2014": 125
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP22",
+        "sampleLat": 37.53191326,
+        "sampleLon": -76.88206986,
+        "p_2013": 53,
+        "p_removal_2013": 56.62,
+        "p_rec_2013": 60,
+        "p_expected_2014": 56.38,
+        "p_actual_2014": 72
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP23",
+        "sampleLat": 37.53250656,
+        "sampleLon": -76.88224999,
+        "p_2013": 37,
+        "p_removal_2013": 71.44,
+        "p_rec_2013": 60,
+        "p_expected_2014": 25.56,
+        "p_actual_2014": 68
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP24",
+        "sampleLat": 37.53299178,
+        "sampleLon": -76.88241996,
+        "p_2013": 40,
+        "p_removal_2013": 56.62,
+        "p_rec_2013": 60,
+        "p_expected_2014": 43.38,
+        "p_actual_2014": 73
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP25",
+        "sampleLat": 37.53352831,
+        "sampleLon": -76.88260005,
+        "p_2013": 68,
+        "p_removal_2013": 68.4,
+        "p_rec_2013": 40,
+        "p_expected_2014": 39.6,
+        "p_actual_2014": 98
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP26",
+        "sampleLat": 37.53357338,
+        "sampleLon": -76.88327161,
+        "p_2013": 62,
+        "p_removal_2013": 52.06,
+        "p_rec_2013": 0,
+        "p_expected_2014": 9.94,
+        "p_actual_2014": 114
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP27",
+        "sampleLat": 37.53299179,
+        "sampleLon": -76.88307823,
+        "p_2013": 77,
+        "p_removal_2013": 31.54,
+        "p_rec_2013": 40,
+        "p_expected_2014": 85.46,
+        "p_actual_2014": 88
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP28",
+        "sampleLat": 37.53247673,
+        "sampleLon": -76.88291994,
+        "p_2013": 87,
+        "p_removal_2013": 46.36,
+        "p_rec_2013": 60,
+        "p_expected_2014": 100.64,
+        "p_actual_2014": 85
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP29",
+        "sampleLat": 37.53166994,
+        "sampleLon": -76.88272662,
+        "p_2013": 68,
+        "p_removal_2013": 49.02,
+        "p_rec_2013": 40,
+        "p_expected_2014": 58.98,
+        "p_actual_2014": 101
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP30",
+        "sampleLat": 37.53162873,
+        "sampleLon": -76.88327119,
+        "p_2013": 75,
+        "p_removal_2013": 63.08,
+        "p_rec_2013": 40,
+        "p_expected_2014": 51.92,
+        "p_actual_2014": 86
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP31",
+        "sampleLat": 37.5321985,
+        "sampleLon": -76.88340992,
+        "p_2013": 57,
+        "p_removal_2013": 70.68,
+        "p_rec_2013": 60,
+        "p_expected_2014": 46.32,
+        "p_actual_2014": 58
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP32",
+        "sampleLat": 37.53273326,
+        "sampleLon": -76.88353334,
+        "p_2013": 57,
+        "p_removal_2013": 69.16,
+        "p_rec_2013": 60,
+        "p_expected_2014": 47.84,
+        "p_actual_2014": 81
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP33",
+        "sampleLat": 37.53328998,
+        "sampleLon": -76.88372501,
+        "p_2013": 58,
+        "p_removal_2013": 47.88,
+        "p_rec_2013": 0,
+        "p_expected_2014": 10.12,
+        "p_actual_2014": 115
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP34",
+        "sampleLat": 37.53387826,
+        "sampleLon": -76.88390827,
+        "p_2013": 43,
+        "p_removal_2013": 59.28,
+        "p_rec_2013": 60,
+        "p_expected_2014": 43.72,
+        "p_actual_2014": 81
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP35",
+        "sampleLat": 37.53441169,
+        "sampleLon": -76.88404659,
+        "p_2013": 65,
+        "p_removal_2013": 61.18,
+        "p_rec_2013": 60,
+        "p_expected_2014": 63.82,
+        "p_actual_2014": 74
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP36",
+        "sampleLat": 37.53521164,
+        "sampleLon": -76.88427331,
+        "p_2013": 62,
+        "p_removal_2013": 57.38,
+        "p_rec_2013": 60,
+        "p_expected_2014": 64.62,
+        "p_actual_2014": 66
+    },
+    {
+        "fieldName": "LIP",
+        "sampleName": "LIP37",
+        "sampleLat": 37.53175662,
+        "sampleLon": -76.88387502,
+        "p_2013": 65,
+        "p_removal_2013": 65.74,
+        "p_rec_2013": 0,
+        "p_expected_2014": -0.74,
+        "p_actual_2014": 128
+    }
+];
+
+function initMap() {
+    const mapCenter = getMapCenter(lipData);
+    const map = openMapAtCenterPoint(mapCenter);
+    lipData.forEach(s => createMarkerForSample(map, s));
+}
+
+function getMapCenter(samples) {
+    const numSamples = samples.length;
+    const avgLat = samples.reduce((acc, s) => acc + s.sampleLat, 0) / numSamples;
+    const avgLon = samples.reduce((acc, s) => acc + s.sampleLon, 0) / numSamples;
+    return {
+        lat: avgLat,
+        lng: avgLon
+    };
+}
+
+function openMapAtCenterPoint(centerPoint) {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: centerPoint,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+    return map;
+}
+
+function createMarkerForSample(map, sample) {
+    var marker = new google.maps.Marker({
+        position: {
+            lat: sample.sampleLat,
+            lng: sample.sampleLon
+        },
+        map: map,
+        title: sample.sampleName,
+        label: sample.sampleName
+    });
+
+    var sampleInfoWindow = createInfoWindowForSample(sample);
+    marker.addListener('click', () => {
+        sampleInfoWindow.open(map, marker);
+        drawChartsForSample(sample);
+    });
+
+    return marker;
+}
+
+function createInfoWindowForSample(sample) {
+    return new google.maps.InfoWindow({
+        content: '<b>' + sample.sampleName + '</b><br>' +
+            '2013 P level: ' + sample.p_2013 + '<br>' +
+            '2013 P rec: ' + sample.p_rec_2013 + '<br>' +
+            '2013 P removal: ' + sample.p_removal_2013
+    });
+}
+
+function drawChartsForSample(sample) {
+    clearChart();
+
+    const chartSelection = d3.select('#chart').data([sample]);
+
+    chartSelection
+        .append('h2')
+        .text(s => s.sampleName);
+
+    chartSelection
+        .append('div')
+        .attr('id', 'bar-wrapper')
+        .style('display', 'inline-flex');
+
+    const barWrapper = d3.select('#bar-wrapper').data([sample]);
+
+    const barWidth = '70px';
+
+    barWrapper
+        .append('div')
+        .attr('id', 'bar-1')
+        .style('display', 'inline-flex')
+        .style('flex-direction', 'column')
+        .style('justify-content', 'flex-end')
+        .style('margin-right', '8px');
+
+    const bar1 = d3.select('#bar-1').data([sample]);
+
+    bar1
+        .append('p')
+        .text(s => (s.p_2013 + s.p_rec_2013) + ' lbs/a');
+
+    bar1
+        .append('div')
+        .style('height', s => (s.p_rec_2013 * 2) + 'px')
+        .style('width', barWidth)
+        .style('background-color', 'yellow')
+        .text('2013 P\nrec');
+
+    bar1
+        .append('div')
+        .style('height', s => (s.p_2013 * 2) + 'px')
+        .style('width', barWidth)
+        .style('background-color', 'lightgreen')
+        .text('2013 P');
+
+    barWrapper
+        .append('div')
+        .attr('id', 'bar-2')
+        .style('display', 'inline-flex')
+        .style('flex-direction', 'column')
+        .style('justify-content', 'flex-end')
+        .style('margin-right', '8px');
+
+    const bar2 = d3.select('#bar-2').data([sample]);
+
+    bar2
+        .append('p')
+        .text(s => s.p_removal_2013 + ' lbs/a');
+
+    bar2
+        .append('div')
+        .style('height', s => (s.p_removal_2013 * 2) + 'px')
+        .style('width', barWidth)
+        .style('background-color', 'palevioletred')
+        .text('2013 P\nremoval');
+}
+
+function clearChart() {
+    const el = document.getElementById('chart');
+    while (el.firstChild) {
+        el.removeChild(el.firstChild);
+    }
+}
